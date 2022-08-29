bc011ca5fd5db79671995ca1bcf01aa582e60929
e-seng <eth.sengsavang@gmail.com>
2022-08-29 13:50:04 -0600

    Add base program source code

---
+++ challenges/reverse-engineering/eseng-hardreveng/source/shift_satellite.c
@@ -0,0 +1,17 @@
+/**
+ * shift_satellite.c
+ * developed by e-seng for magpieCTF 2023
+ *
+ * this binary will act as the server backend of some client. inputs received
+ * are expected to be encoded by the client and then interpreted here on the
+ * server.
+ *
+ * between some command station and the receiving station where the challenger
+ * is connected two are three satellites. due to time, the dishes of the
+ * satellite have become misaligned. as such, to collect the secrets from the
+ * source, the dishes must be realigned.
+ *
+ * in a "practical/applied" setting, a user would interact with the client program
+ * which will then send this server binary what to do. thus, the server's inputs
+ * are obfuscated for "simplified" transmission (or something)
+ */
