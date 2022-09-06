cd92f7ce13833181a18cda2adfba884e01436272
e-seng <eth.sengsavang@gmail.com>
2022-09-06 11:22:08 -0600

    Define Satellite attributes and methods for simulation

--- challenges/reverse-engineering/eseng-hardreveng/source/simulate_satellites.py
+++ challenges/reverse-engineering/eseng-hardreveng/source/simulate_satellites.py
@@ -16,12 +16,46 @@ output as specified below.
 import random
 import subprocessing as subp
 
+# simulation details
 NO_SATELLITES = 4
 MAX_ANGLE = 360.0
+GRID_SIZE = 12
 
 class Satellite:
+    _name_base = "Satellite "
+
     def __init__(self):
-        self.theta = (-1,-1,-1)
+        self.theta = (-1,-1,-1) # -1 indicates un-initialized
         self.pos = (-1,-1,-1)
         self.name = ""
         self.symbol = ''
+
+    def _orient_angle(self, tx, ty, tz):
+        """
+        orients the angle of the Satellite to the tuple `theta = (tx, ty, tz)`.
+
+        for any angular component x, y or z in theta = (x, y, z), the component
+        must be within the range of [0, 360) in degrees. this is a cyclical
+        range
+        """
+        self.theta[0] = tx % MAX_ANGLE
+        self.theta[1] = ty % MAX_ANGLE
+        self.theta[2] = tz % MAX_ANGLE
+
+    def add_angle(self, dtx, dty, dtz):
+        """
+        adds the specified angle to the Satellite, changing its orientation.
+        this will call self._orient_angle
+        """
+        self._orient_angle(self.theta[0]+dtx, self.theta[1]+dty, self.theta[2]+dtz)
+
+    def set_satellite(self, theta, pos, symbol):
+        """
+        sets the details of the satellite
+
+        this should be used when generating satellite information
+        """
+        self.theta = tuple([t%MAX_ANGLE for t in theta])
+        self.pos = tuple([p%GRID_SIZE for p in pos])
+        self.name = self._name_base += symbol
+        self.symbol = symbol
